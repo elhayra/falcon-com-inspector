8ced9ad7f8e7ede4f2eb9354baea2af67528e01f
elhayr1 <elhayr1@gmail.com>
2018-08-04 19:31:04 +0200

    package wiz

--- gui/Falcon/PackageWizard/Package.cs
+++ gui/Falcon/PackageWizard/Package.cs
@@ -8,5 +8,31 @@ namespace Falcon.PackageWizard
 {
     class Package
     {
+        public enum EndianTypeEnum
+        {
+            BIG,
+            SMALL
+        }
+
+        public enum FieldTypeEnum
+        {
+            UINT8,
+            INT8, 
+            UINT16,
+            INT16, 
+            UINT32,
+            INT32,
+            UINT64,
+            INT64,
+            FLOAT32, 
+            FLOAT64,
+            STRING
+        }
+
+        //private List<fields> headerFields = new List<fields>();
+
+        //private List<fields> dataFields = new List<fields>();
+
+        //public void AddField()
     }
 }
--- gui/Falcon/PackageWizard/PackageWizard.cs
+++ gui/Falcon/PackageWizard/PackageWizard.cs
@@ -35,60 +35,101 @@ namespace Falcon.PackageWizard
         }
 
         /// <summary>
-        /// Handle a not INVALID section
+        /// Handle a valid section
         /// </summary>
         /// <param name="section"></param>
-        private static void HandleSection(Section section, ref StreamReader reader)
+        //private static bool HandleSection(Section section, ref StreamReader reader, ref Package pkg)
+        //{
+        //    switch (section)
+        //    {
+        //        case Section.ENDIANESS:
+        //            return HandleEndianess(ref reader, ref pkg);
+
+        //        case Section.HEADER:
+
+
+        //        case Section.FIELDS:
+
+
+        //    }
+        //}
+
+        private static bool HandleEndianess(ref StreamReader reader, ref Package pkg)
         {
-            switch (section)
+            string line = reader.ReadLine();
+            if (line != null)
             {
-                case Section.ENDIANESS:
-
-                    break;
-                case Section.HEADER:
-
-                    break;
-                case Section.FIELDS:
-
-                    break;
-            }
-        }
-
-        public static Package GeneratePackageFromFile(string file)
-        {
-            Section section = Section.NONE;
-            var reader = new StreamReader(file);
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                // ignore comments and empty lines
-                if (!line.StartsWith("#") && line != "") 
+                if (line == "big-endian")
                 {
-                    if (section == Section.NONE)
-                    {
-                        section = StringToSection(line);
-
-                        if (section == Section.INVALID)
-                        {
-                            reader.Close();
-                            var argExp = new ArgumentException("A section name must come before data attributes", line); 
-                            throw argExp;
-                        }
-
-                        HandleSection(section, ref reader);
-                    }
-                        
+                    // TODO: update package endian
+                    return true;
+                }
+                if (line == "small-endian")
+                {
+                    // TODO: update package endian
+                    return true;
                 }
 
             }
-            reader.Close();
-
-            return null;
+            return false;
         }
 
-        public static string ParseBytesPackage(ref Package pkg, byte [] bytes)
-        {
+        //private static bool HandleHeader(ref StreamReader reader, ref Package pkg)
+        //{
+        //    string line = reader.ReadLine();
+        //    while ((line = reader.ReadLine()) != null)
+        //    {
+                
+        //    }
+        //}
 
-        }
+        //private static bool HandleFields(ref StreamReader reader, ref Package pkg)
+        //{
+        //    string line = reader.ReadLine();
+        //    while ((line = reader.ReadLine()) != null)
+        //    {
+
+        //    }
+        //}
+
+        //public static Package GeneratePackageFromFile(string file)
+        //{
+        //    var package = new Package();
+        //    Section section = Section.NONE;
+        //    var reader = new StreamReader(file);
+        //    string line;
+        //    while ((line = reader.ReadLine()) != null)
+        //    {
+        //        // ignore comments and empty lines
+        //        if (!line.StartsWith("#") && line != "") 
+        //        {
+        //            if (section == Section.NONE)
+        //            {
+        //                section = StringToSection(line);
+
+        //                if (section == Section.INVALID)
+        //                {
+        //                    reader.Close();
+        //                    var argExp = new ArgumentException("A section name must come before data attributes", line); 
+        //                    throw argExp;
+        //                }
+
+        //                HandleSection(section, ref reader);
+
+        //                section = Section.NONE;
+        //            }
+                        
+        //        }
+
+        //    }
+        //    reader.Close();
+
+        //    return null;
+        //}
+
+        //public static string ParseBytesPackage(ref Package pkg, byte [] bytes)
+        //{
+
+        //}
     }
 }
